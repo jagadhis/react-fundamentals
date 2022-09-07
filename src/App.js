8634f58616c8274b5e34704faca1923cb90f01e2
jagadhis <jwaran78@gmail.com>
2022-09-08 05:08:59 +0530

    LifeCycle

--- src/App.js
+++ src/App.js
@@ -10,16 +10,20 @@ const [count ,setCount] = useState(0);
 const [windowWidth,setwindowWidth] = useState(window.innerWidth);
 const [name,setName] = useState("");
 
-
+{/* on Every Render*/}
 useEffect(() => {
   console.log("Re-rendered");
 })
   
-
 {/*componentDidMount alternative renderes only one time when the Component mounts */}
 useEffect(() => {
 console.log("Mounted");
 },[])
+
+{/*componentDidUpdate alternative renderes on start when the dependency array changes*/}
+useEffect(()=>{
+  console.log(`the name is changed: ${name}`)
+},[name])
 
 const increment=() => {
   setCount(count + 1)
@@ -30,11 +34,24 @@ const decrement=() => {
   return (
     <div className="App">
 
+    
+
         {/* State Example*/} 
       <h1>A Counter App</h1>
       <p>The Count is : {count}</p>
       <button onClick={increment}>Add</button>
       <button onClick={decrement}>Less</button>
+      <br/>
+      <br/>
+
+      {/* useEffect Example*/} 
+      <div>
+      <input 
+      value={name} 
+      onChange= {(e) => setName(e.target.value)}
+      placeholder="enter value" 
+      />
+      </div>
 
 
         {/* Normal Props */}
