51f348e8d9ee6bb372cd0d586aa57dc29ce3a39f
jagadhis <jwaran78@gmail.com>
2022-09-08 02:26:22 +0530

    Counter

--- src/App.js
+++ src/App.js
@@ -3,8 +3,21 @@ import Item from './Item';
 import Product from './Product';
 
 function App() {
+let count=0;
+
+const increment=() => {
+  count += 1;
+}
+const decrement=() => {
+  count -= 1;
+}
   return (
     <div className="App">
+
+      <h1>A Counter App</h1>
+      <p>The Count is : {count}</p>
+      <button>Add</button>
+      <button>Less</button>
         {/* Normal Props */}
       <Product name="Apple Iphone 13" description="Iphone" price={799} />
         {/* destructuring Props */}
