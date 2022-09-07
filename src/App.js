91af81d0a0a16ef1b041ed126e8a7af09ff0fbe5
jagadhis <jwaran78@gmail.com>
2022-09-07 08:25:31 +0530

    Props Passing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Product from './Product';
 
 function App() {
   return (
     <div className="App">
-
+      <Product name="Apple Iphone 13" description="Iphone" price={799} />
     </div>
   );
 }
